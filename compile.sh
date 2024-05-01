cab4c4a97d9c5414f4ce72d3b9400d8e31068102
canaria-computer <121685177+canaria-computer@users.noreply.github.com>
2024-05-01 19:59:59 +0900

    compile misconfig fix

--- compile.sh
+++ compile.sh
@@ -14,12 +14,8 @@ GOOS=netbsd    GOARCH=amd64 go build -o ./dist/netbsd/amd64/
 GOOS=netbsd    GOARCH=arm   go build -o ./dist/netbsd/arm/
 GOOS=openbsd   GOARCH=386   go build -o ./dist/openbsd/386/
 GOOS=openbsd   GOARCH=amd64 go build -o ./dist/openbsd/amd64/
-GOOS=plan9     GOARCH=386   go build -o ./dist/plan9/386/
-GOOS=plan9     GOARCH=amd64 go build -o ./dist/plan9/amd64/
 GOOS=windows   GOARCH=386   go build -o ./dist/windows/386/
 GOOS=windows   GOARCH=amd64 go build -o ./dist/windows/amd64/
-GOOS=nacl      GOARCH=amd64 go build -o ./dist/nacl/amd64/
-GOOS=nacl      GOARCH=386   go build -o ./dist/nacl/386/
 
 7z a -mx=5 ./dist/darwin.zip ./dist/darwin/
 7z a -mx=5 ./dist/darwin.7z ./dist/darwin/
@@ -31,9 +27,5 @@ GOOS=nacl      GOARCH=386   go build -o ./dist/nacl/386/
 7z a -mx=5 ./dist/netbsd.7z ./dist/netbsd/
 7z a -mx=5 ./dist/openbsd.zip ./dist/openbsd/
 7z a -mx=5 ./dist/openbsd.7z ./dist/openbsd/
-7z a -mx=5 ./dist/plan9.zip ./dist/plan9/
-7z a -mx=5 ./dist/plan9.7z ./dist/plan9/
 7z a -mx=5 ./dist/windows.zip ./dist/windows/
 7z a -mx=5 ./dist/windows.7z ./dist/windows/
-7z a -mx=5 ./dist/nacl.zip ./dist/nacl/
-7z a -mx=5 ./dist/nacl.7z ./dist/nacl/
